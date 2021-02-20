d37f10373b56f848f6e5711280e4bb2c50d09cb2
Ruby Berumen <clararubipb15@gmail.com>
2021-02-19 19:32:43 -0600

    Se agregaron las operaciones para mostrar el resultado

--- src/PruebaConversorTemperaturas.java
+++ src/PruebaConversorTemperaturas.java
@@ -31,7 +31,7 @@ class Conversor extends JFrame implements ActionListener{
 		add(new JLabel("A: "));
 		
 		temps2 = new JComboBox<>();
-		temps2.addItem("Elige opcion...");
+		temps2.addItem("Elige opción...");
 		add(temps2);
 		
 		add(new JLabel("="));
@@ -46,43 +46,78 @@ class Conversor extends JFrame implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-	
+		
 		String tC [] = {"°Fahrenheit", "°Kelvin", "°Rankine"};
+		String tF [] = {"°Centigrados", "°Kelvin", "°Rankine"};
+		String tK [] = {"°Centigrados", "°Fahrenheit", "°Rankine"};
+		String tR [] = {"°Centigrados", "°Fahrenheit", "°Kelvin"};
+		
+		
 		if(e.getSource() == temps1) {
 			temps2.removeAllItems();
 			if(temps1.getSelectedItem().equals("°Centigrados")) {
 				for( String x : tC)
 					temps2.addItem(x);
-			}
-		}
+				
+				double n1 = Double.parseDouble(caja1.getSelectedText());
+				double res = 0.0;
+				if(temps2.getSelectedItem().equals("°Fahrenheit")) {
+					res = (n1*1.8)+32;
+				}else if(temps2.getSelectedItem().equals("°Kelvin")) {
+					res = n1+273.15;
+				}else if(temps2.getSelectedItem().equals("°Rankine")) {
+					res = (n1*1.8)+493.47;
+				}
+				cajaRes.setText(String.valueOf(res));
+				
 		
-		
-		String tF [] = {"°Centigrados", "°Kelvin", "°Rankine"};
-		if(e.getSource() == temps1) {
-			temps2.removeAllItems();
-			if(temps1.getSelectedItem().equals("°Fahrenheit")) {
+			}else if(temps1.getSelectedItem().equals("°Fahrenheit")) {
 				for( String x : tF)
 					temps2.addItem(x);
-			}
-		}
-		
-		
-		String tK [] = {"°Centigrados", "°Fahrenheit", "°Rankine"};
-		if(e.getSource() == temps1) {
-			temps2.removeAllItems();
-			if(temps1.getSelectedItem().equals("°Kelvin")) {
+				
+				double n1 = Double.parseDouble(caja1.getSelectedText());
+				double res = 0.0;
+				if(temps2.getSelectedItem().equals("°Centigrados")) {
+					res = ((n1-32)*(5/9));
+				}else if(temps2.getSelectedItem().equals("°Kelvin")) {
+					res = ((n1-32)*(5/9)+273.15);
+				}else if(temps2.getSelectedItem().equals("°Rankine")) {
+					res = (n1+469.67);
+				}
+				cajaRes.setText(String.valueOf(res));
+				
+				
+			}else if(temps1.getSelectedItem().equals("°Kelvin")) {
 				for( String x : tK)
 					temps2.addItem(x);
-			}
-		}
-		
-		
-		String tR [] = {"°Centigrados", "°Fahrenheit", "°Kelvin"};
-		if(e.getSource() == temps1) {
-			temps2.removeAllItems();
-			if(temps1.getSelectedItem().equals("°Rankine")) {
+				
+				double n1 = Double.parseDouble(caja1.getSelectedText());
+				double res = 0.0;
+				if(temps2.getSelectedItem().equals("°Centigrados")) {
+					res = (n1-273.15);
+				}else if(temps2.getSelectedItem().equals("°Fahrenheit")) {
+					res = ((n1-273.15)*(9/5)+32);
+				}else if(temps2.getSelectedItem().equals("°Rankine")) {
+					res = (n1*1.8);
+				}
+				cajaRes.setText(String.valueOf(res));
+				
+				
+			}else if(temps1.getSelectedItem().equals("°Rankine")) {
 				for( String x : tR)
 					temps2.addItem(x);
+				
+				double n1 = Double.parseDouble(caja1.getSelectedText());
+				double res = 0.0;
+				if(temps2.getSelectedItem().equals("°Centigrados")) {
+					res = ((n1-491.67)*(5/9));
+				}else if(temps2.getSelectedItem().equals("°Fahrenheit")) {
+					res = (n1-459.67);
+				}else if(temps2.getSelectedItem().equals("°Kelvin")) {
+					res = (n1*(5/9));
+				}
+				cajaRes.setText(String.valueOf(res));
+				
 			}
 		}
 		
